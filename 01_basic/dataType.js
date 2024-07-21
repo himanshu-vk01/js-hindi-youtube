7bcf3b8e052292367cf836e4ea72abae58893012
Himanshu sinha <hmraj4141@gmail.com>
2024-07-21 12:36:03 +0000

    datatype

---
+++ 01_basic/dataType.js
@@ -0,0 +1,20 @@
+"use strict";
+//alert(3+3) we cant use rightnow
+
+
+
+//number = 2 to power 54
+//bigint
+//string = ""
+//boolean = true/false
+//null  =  standlonalone value
+//undfined
+//symbol => unique
+
+
+
+// non premitive
+
+
+
+//object
