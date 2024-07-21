aeb9f1632a973f55491d6cbab6392127b08b476d
Himanshu sinha <hmraj4141@gmail.com>
2024-07-21 11:17:34 +0000

    container config done

---
+++ 01_basic/variable.js
@@ -0,0 +1,7 @@
+const numbr = 1234321
+var name ="Himanshu"
+let lastName = "Sinha"
+Home = "diwanganj"
+console.table([numbr , name , lastName , Home])
+
+/* we cant use var variable due to scope problem*/
