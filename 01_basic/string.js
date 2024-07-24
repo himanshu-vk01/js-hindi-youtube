30011a996f3ea1050185d1246ce89740416cf191
Himanshu sinha <hmraj4141@gmail.com>
2024-07-24 14:04:27 +0000

    string

---
+++ 01_basic/string.js
@@ -0,0 +1,21 @@
+const name = 'Himanshu RAJ'
+let age = 18
+
+const home = new String("Supaul")
+ 
+//console.log("my name is " + name + " and my age is "+ age)
+
+//console.log(` Hello my name is ${name} and i am from ${home} and my age is ${age} `);
+
+
+
+/// function :->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+console.log(name.length);  //  for know the length of string   :----->>> 12
+console.log(name.toUpperCase())  // for covert string to upercase string  :--->>  HIMANSHU RAJ
+console.log(name.charAt(5));  // find the charter of that number position
+console.log(name.indexOf('R'));  // find that index of that chartter of that string
+console.log(name.substring(0,4));  // slice the  string into part parameter ==  where we want to  start the slice and second one for last postion but not included
+console.log(name.slice(0,3))  /// same as substring but here we can include negative number so slicing start from last
+//console.log(name.replace(Himanshu , vikash))
